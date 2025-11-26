ddff8b47d18df68e1473056afb12ebcb35db975c
Ynez Dias <ynezdias0501@gmail.com>
2025-11-25 22:52:47 -0500

    Chnages in imports

--- authnft/contracts/CertificateNFT.sol
+++ authnft/contracts/CertificateNFT.sol
@@ -3,19 +3,16 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-contract CertificateNFT is ERC721URIStorage, AccessControl {
 
+contract CertificateNFT is ERC721URIStorage, AccessControl {
     uint256 private _nextTokenId = 1;
 
     bytes32 public constant ISSUER_ROLE = keccak256("ISSUER_ROLE");
 
-    event CertificateMinted(address indexed issuer, address indexed to, uint256 tokenId, string tokenURI);
-
-constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
-    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    _setupRole(ISSUER_ROLE, msg.sender);
-}
-
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(ISSUER_ROLE, msg.sender);
+    }
 
     function grantIssuer(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
         grantRole(ISSUER_ROLE, account);
@@ -26,15 +23,14 @@ constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
     }
 
     function mintCertificate(address to, string calldata tokenURI) external onlyRole(ISSUER_ROLE) returns (uint256) {
-        uint256 newId = _nextTokenId;
-        _mint(to, newId);
-        _setTokenURI(newId, tokenURI);
+        uint256 tokenId = _nextTokenId;
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
         _nextTokenId++;
-        emit CertificateMinted(msg.sender, to, newId, tokenURI);
-        return newId;
+        return tokenId;
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721URIStorage, AccessControl) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721URIStorage, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
